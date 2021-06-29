8026c7813fc75497d6b954ec9bd41e16a13faab8
Kimyata Graves <yata09@gmail.com>
2021-06-28 22:38:48 -0500

    Added enter keypress

--- todolist.js
+++ todolist.js
@@ -1,18 +1,23 @@
-//Create a new list item when clicking add
+//declaring variables//
 let addToDoButton = document.getElementById('addButn');
 let itemsToDo = document.getElementById('itemsToDo');
 let inputField = document.getElementById('myInput');
 
-addToDoButton.addEventListener('click', function () {
-  var list = document.createElement('p');
-  list.innerText = inputField.value;
-  list.classList.add(); //add css class name here//
-  itemsToDo.appendChild(list);
-  inputField.value = '';
+addToDoButton.addEventListener('click', function makeList() {
+  var list = document.createElement('p'); //create paragraph everytime button is clicked//
+  list.innerText = inputField.value; //sets text of paragraph = value that is inputed//
+  itemsToDo.appendChild(list); //appends list to itemsToDo//
+  inputField.value = ''; //removes the input after it is added to the list//
   list.addEventListener('click', function () {
-    list.style.textDecoration = 'line-through';
+    list.classList.add('doneElement'); //item done//
   });
   list.addEventListener('dblclick', function () {
-    itemsToDo.removeChild(list);
+    itemsToDo.removeChild(list); //removes item after completed//
+  });
+  //items can be added by pressing enter key//
+  inputField.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+      makeList();
+    }
   });
 });
